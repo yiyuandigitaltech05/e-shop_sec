e9efd1cf9ce9d29d62c910c74f8df9d62575a92a
yiyuandigital05 <yiyuandigitaltech05@gmail.com>
2024-06-07 17:44:11 +0800

    add

--- src/main/Checkout.jsx
+++ src/main/Checkout.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import {Container,Row,Col,Form,FormGroup} from"reactstrap"
+import Helmen from "./Helmen";
+import CommonSection from "./UI/CommonSection";
+import "../Style/Checkout.css"
+import { useSelector } from "react-redux";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+
+  const totalQty = useSelector(state=>state.cart.totalQuantity);
+  const totalAmount = useSelector(state=>state.cart.totalAmount)
+
+  return <Helmen title="Checkout">
+    <CommonSection title="Checkout"/>
+    <section className="Checkout_Wrap">
+      <Container>
+        <Row>
+          <Col lg='8'>
+            <h6 className="mb-4 fw-bold">Billing Information</h6>
+            <Form>
+              <FormGroup>
+                <input type="text" placeholder="Enter your name" />
+              </FormGroup>
+              <FormGroup>
+                <input type="email" placeholder="Enter your email" />
+              </FormGroup>
+              <FormGroup>
+                <input type="number" placeholder="Phone number" />
+              </FormGroup>
+              <FormGroup>
+                <input type="text" placeholder="Street address" />
+              </FormGroup>
+              <FormGroup>
+                <input type="text" placeholder="City" />
+              </FormGroup>
+              <FormGroup>
+                <input type="text" placeholder="Postal Code" />
+              </FormGroup>
+              <FormGroup>
+                <input type="text" placeholder="Country" />
+              </FormGroup>
+            </Form>
+          </Col>
+          <Col lg='4'>
+            <div className="checkout__cart ">
+              <h6>Total Qty: <span>{totalQty} {totalQty>=2? "items" :"item"}</span></h6>
+              <h6 className="my-3">Subtotal: <span>${totalAmount}</span></h6>
+              <h6 className="my-2"><span>Shipping: <br />free Shipping</span> <span>$0</span></h6>
+              <hr />
+              <h4 className="my-2">Total Cost: <span>${totalAmount}</span></h4>
+              <button className="clock_buy text-black bg-slate-100 w-100 mx-auto block">Place an order</button>
+            </div>
+            
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  </Helmen>;
 };
 
 export default Checkout;
--- src/main/Login.jsx
+++ src/main/Login.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Helmen from "./Helmen";
+import {Container ,Row ,Col , Form, FormGroup} from "reactstrap"
+import { Link } from "react-router-dom";
+import "../Style/Login.css"
 
 const Login = () => {
-  return <div>Login</div>;
+  return <Helmen title="Login">
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6' className="m-auto text-center">
+          <h3 className="fw-bold fs-4">Login</h3>
+          
+
+          <Form className="auth_form">
+            <FormGroup className="form_group">
+              <input type="email" placeholder="Enter your email"/>
+            </FormGroup>
+            <FormGroup className="form_group">
+              <input type="password" placeholder="Enter your password"/>
+            </FormGroup>
+            <button className="clock_buy auth_btn mx-auto block">Login</button>
+          </Form>
+          <p className="text-slate-400">Don't have an account? <Link to="/signup">Create an account</Link></p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  </Helmen>;
 };
 
 export default Login;
